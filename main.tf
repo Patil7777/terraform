ce4fdec2dd6e07427a61dd002a20b27cbe56f1fa
shubham <shubhampatil28.1999@gmail.com>
2024-02-15 00:19:15 +0530

    update file

---
+++ main.tf
@@ -0,0 +1,37 @@
+provider    "aws"   {
+    region =    "eu-west-2"
+}
+
+resources   "aws_instance"  "my-terraform-instance" {
+    ami =   "ami-0e5f882be1900e43b"
+    instance_type   =   "t2.micro"
+
+    //Associate the instance with the security group
+    security_groups =   [aws_security_groups.my-terraform-instance]
+}
+
+
+resource "aws_security_gropup"  "my_sg" {
+    name   =    "my-terrafrom-security"
+    description =    "allow SSH and HTTP traffic"
+}
+
+
+Define ingress rule to allow SSH and HTTP traffic
+ingress {
+    from_port   =   22
+    to_port =   22
+    protocol    =   "TCP"
+    cidr_blocks =   ["0.0.0.0/0"]
+}
+
+ingress {
+    from_port   =   80
+    to_port=    80
+    protocol=   "TCP"
+    cide_blocks =   ["0.0.0.0/0"]
+}
+
+
+
+
