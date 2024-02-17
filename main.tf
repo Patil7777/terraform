5e996472107658616672991be40a8ba44dcbcc75
shubham <shubhampatil28.1999@gmail.com>
2024-02-17 21:42:32 +0530

    upadted now

--- main.tf
+++ main.tf
@@ -5,17 +5,19 @@ provider    "aws"   {
 resource   "aws_instance"  "my-terraform-instance" {
     ami =   "ami-0e5f882be1900e43b"
     instance_type   =   "t2.micro"
-    tags[
+
+
+    tags =  {
         name    =   "my-instance"
         env     =   "dev"
-    ]
+    }
 }
 
 resource     "aws_security_group"  "my_sg" {
     name   =    "my-terrafrom-security"
     description =    "allow ssh and http traffic"
     vpc_id  =   var.vpc_id
-}
+
 ingress {
     from_port   =   22
     to_port     =   22
@@ -28,19 +30,18 @@ ingress {
     protocol=   "TCP"
     cide_blocks =   ["0.0.0.0/0"]
 }
+
 egress  {
     from_port   =   0
     to_port     =    0
     protocol       =   "-1"
     cidr_blocks =   ["0.0.0.0/0"]
+    }
 }   
 
 
 
-# Variables {
+# Variables 
+variable "vpc_id"   {
     default =   "vpc-0dd0557072ef81cd7"
 }
-
-
-
-
